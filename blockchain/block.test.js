e2c9ae67849ea00575b807965f2c12d7e620bc3c
Raymond Ho <chunkiat82@gmail.com>
2020-04-20 21:16:04 +0800

    Difficulty added

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -2,10 +2,9 @@ const Block = require('./block');
 const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
-
   let data, lastBlock, block;
 
-  beforeEach(()=>{
+  beforeEach(() => {
     data = 'bar';
     lastBlock = Block.gensis();
     block = Block.mineBlock(lastBlock, data);
@@ -13,13 +12,19 @@ describe('Block', () => {
 
   it('sets the `data` to match the input', () => {
     expect(block.data).toEqual(data);
-  })
+  });
 
   it('sets the `last hash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash);
-  })
+  });
 
-  it('it generates a hash that matches the difficulty', ()=> {
+  it('it generates a hash that matches the difficulty', () => {
     expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+  });
+
+  it('lowers the difficulted for slowly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(
+      block.diffculty - 1
+    );
   });
 });
